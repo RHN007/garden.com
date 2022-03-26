fd138f8b1544240480db27f4433fb59275bb7cdf
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-03-26 23:21:09 +0600

    adding react files

---
+++ gardenbd.com/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Store from './components/Store/Store';
+
+function App() {
+  return (
+    <div className="App">
+      <Store></Store>
+    </div>
+  );
+}
+
+export default App;
---
+++ gardenbd.com/src/components/Store/Store.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Store = () => {
+    return (
+        <div>
+            <h1>This is product store </h1>
+        </div>
+    );
+};
+
+export default Store;
